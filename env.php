dec91299eaa3d96c9d1e04e476f287e01e6b8dad
ryoji-U <ryoji.australianopen.090203@gmail.com>
2021-06-16 10:07:09 +0900

    initial commit

--- env.php
+++ env.php
@@ -2,10 +2,10 @@
 
 
 //ローカルxamppのDB情報
-define('DB_HOST','');  //ホスト名
-define('DB_NAME',''); //データベース名
-define('DB_USER','');       //ユーザー名
-define('DB_PASS','');       //パスワード
+define('DB_HOST','');   //ホスト名
+define('DB_NAME','');   //データベース名
+define('DB_USER','');   //ユーザー名
+define('DB_PASS','');   //パスワード
 
 
 ?>
