be1655e2254c46d36bf1d24ccc45b666ad629450
ryoji-U <ryoji.australianopen.090203@gmail.com>
2021-06-16 10:57:21 +0900

    env.phpを編集しました。

--- env.php
+++ env.php
@@ -1,11 +1,9 @@
 ﻿<?php
 
-
 //ローカルxamppのDB情報
 define('DB_HOST','');       //ホスト名
 define('DB_NAME','');       //データベース名
 define('DB_USER','');       //ユーザー名
 define('DB_PASS','');       //パスワード
-
 
 ?>
